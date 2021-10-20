21f1e2886f0044d50eefdccbcbc57fab7b2f5b27
pacosw1 <pacoswilliams@gmail.com>
2021-10-20 10:36:26 -0500

    base

--- app.js
+++ app.js
@@ -1,33 +1,49 @@
-let canvasGL;
-let vertexShaderText = [].join('\n');
-let fragmentShaderText = [].join('\n');
+const scene = new THREE.Scene();
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 
-function start() {
-    let canvas = document.getElementById("canvasGL");
+// create camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.z = 20;
+camera.position.y = 40;
+camera.position.x = 0;
 
-    canvasGL = startWebGL(canvas);
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.update();
 
-    if (canvasGL) {
-        canvasGL.clearColor(0.0, 0.0, 0.0, 1.0);
-        canvasGL.enable(canvasGL.DEPTH_TEST);
-        canvasGL.depthFunc(canvasGL.LEQUAL);
-        canvasGL.clear(canvasGL.COLOR_BUFFER_BIT|canvasGL.DEPTH_BUFFER_BIT);
-    }
+// create plane
+const planeGeometry = new THREE.PlaneGeometry(100, 100);
+const planeMaterial = new THREE.MeshPhongMaterial({color: 0x61904});
+const grass = new THREE.Mesh(planeGeometry, planeMaterial);
+grass.receiveShadow = true;
+grass.rotation.x = -0.5 * Math.PI;
+scene.add(grass);
+
+// create light
+const light = new THREE.DirectionalLight( 0xEEFFD3, 10);
+light.castShadow = true;
+light.position.set(0, 50, 10);
+scene.add( light );
+
+
+
+
+for(let i = 0; i < 10; i++) {
+    const trunkGeometry = new THREE.BoxGeometry( 1, 1, 1);
+    const trunkMaterial = new THREE.MeshPhongMaterial( { color: 0x3f330c} );
+    const trunk = new THREE.Mesh( trunkGeometry, trunkMaterial );
+    trunk.position.set(Math.random() * 20 * Math.PI, 0.5, (Math.random() * 20 * Math.PI));
+    scene.add(trunk);
 }
 
-function startWebGL(canvas) {
-    canvasGL = null
 
-    try {
-        canvasGL = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-    }
-    catch (e) {}
-
-    if (!canvasGL) {
-        alert("No se puede inicializar WebGL. El navegador no lo soporta.");
-        canvasGL = null;
-    }
-
-    return canvasGL;
+function render() {
+    requestAnimationFrame(render);
+    controls.update();
+    renderer.render(scene, camera);
 }
+
+render();
