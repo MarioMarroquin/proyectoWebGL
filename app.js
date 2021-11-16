1b4f7bbfb5714758e04ed5309a3557c77eac85c1
Mario Marroquin <a00825266@itesm.mx>
2021-11-15 21:34:44 -0600

    Added textures and live fire.
    Other improvements to code.

--- app.js
+++ app.js
@@ -1,98 +1,140 @@
-const scene = new THREE.Scene();
+const renderTree = () => {
+  const circleRadius = 10;
 
-const planeDimensions = 100;
-const percentage = 0.97;
+  const trunkGeometry = new THREE.BoxGeometry(1, 1, 1);
+  const trunkMaterial = new THREE.MeshLambertMaterial({map: trunkTexture});
+  const trunk = new THREE.Mesh(trunkGeometry, trunkMaterial);
+
+  const leaveGeometry = new THREE.BoxGeometry(1, 1, 1);
+  const leaveMaterial = new THREE.MeshLambertMaterial({map: leaveTexture});
+  const leave = new THREE.Mesh(leaveGeometry, leaveMaterial);
+
+  let randomSignX = Math.round(Math.random()) === 1 ? 1 : -1;
+  let randomSignY = Math.round(Math.random()) === 1 ? 1 : -1;
+  let treeX = Math.random() * ((planeDimensions * percentage) / 2) * randomSignX;
+  let treeY = Math.random() * ((planeDimensions * percentage) / 2) * randomSignY;
+
+  let distance = Math.sqrt(Math.pow(treeX, 2) + Math.pow(treeY, 2));
+
+  if (distance < circleRadius) return;
+
+  trunk.position.set(treeX, 4, treeY);
+  trunk.scale.set(1, 8, 1);
+
+  leave.position.set(treeX, 8, treeY);
+  leave.scale.set(3, 3.5, 3);
+
+  scene.add(trunk);
+  scene.add(leave);
+};
 
 const renderCampsite = () => {
   const circleRadius = 3;
-  const trunkGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-  const trunkMaterial = new THREE.MeshPhongMaterial({ color: 0x3f330c });
-  const trunk = new THREE.Mesh(trunkGeometry, trunkMaterial);
 
-  const randomSignX = Math.round(Math.random()) === 1 ? 1 : -1;
-  const randomSignY = Math.round(Math.random()) === 1 ? 1 : -1;
-  const treeX =
-    Math.random() * ((planeDimensions * percentage) / 2) * randomSignX;
-  const treeY =
-    Math.random() * ((planeDimensions * percentage) / 2) * randomSignY;
+  const stoneGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+  const stoneMaterial = new THREE.MeshPhongMaterial({ color: 0x3f330c });
+  const stone = new THREE.Mesh(stoneGeometry, stoneMaterial);
 
-  const distance = Math.sqrt(Math.pow(treeX - 0, 2) + Math.pow(treeY - 0, 2));
+  let randomSignX = Math.round(Math.random()) === 1 ? 1 : -1;
+  let randomSignY = Math.round(Math.random()) === 1 ? 1 : -1;
+  let treeX = Math.random() * ((planeDimensions * percentage) / 2) * randomSignX;
+  let treeY = Math.random() * ((planeDimensions * percentage) / 2) * randomSignY;
 
-  console.log(treeX);
+  let distance = Math.sqrt(Math.pow(treeX, 2) + Math.pow(treeY, 2));
+
   if (Math.floor(distance) !== circleRadius) return renderCampsite();
 
-  trunk.position.set(treeX - 0.5, 0.5, treeY - 0.5);
-  return trunk;
+  stone.position.set(treeX, 0, treeY);
+
+  scene.add(stone);
 };
 
-const renderTree = () => {
-  const circleRadius = 10;
-  const trunkGeometry = new THREE.BoxGeometry(1, 1, 1);
-  const trunkMaterial = new THREE.MeshPhongMaterial({ color: 0x3f330c });
-  const trunk = new THREE.Mesh(trunkGeometry, trunkMaterial);
 
-  const randomSignX = Math.round(Math.random()) === 1 ? 1 : -1;
-  const randomSignY = Math.round(Math.random()) === 1 ? 1 : -1;
-  const treeX =
-    Math.random() * ((planeDimensions * percentage) / 2) * randomSignX;
-  const treeY =
-    Math.random() * ((planeDimensions * percentage) / 2) * randomSignY;
+const scene = new THREE.Scene();
+const clock = new THREE.Clock();
 
-  const distance = Math.sqrt(Math.pow(treeX - 0, 2) + Math.pow(treeY - 0, 2));
 
-  console.log(treeX);
-  if (distance < circleRadius) return;
-
-  trunk.position.set(treeX - 0.5, 4, treeY - 0.5);
-  trunk.scale.set(1, 8, 1);
-  return trunk;
-};
-
+// create render
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+
 // create camera
 const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
 );
+
 camera.position.z = 20;
 camera.position.y = 40;
 camera.position.x = 0;
 
+
+// create control to move around scene, center fix.
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.update();
 
+
 // create plane
-const planeGeometry = new THREE.PlaneGeometry(100, 100);
-const planeMaterial = new THREE.MeshPhongMaterial({ color: 0x61904 });
+const planeDimensions = 100;
+const percentage = 0.97;
+
+const planeGeometry = new THREE.PlaneGeometry(100, 100, 50, 50);
+const planeTexture = new THREE.TextureLoader().load('textures/floor.jpg');
+const planeMaterial = new THREE.MeshPhongMaterial({ map: planeTexture});
+planeMaterial.map.wrapS =  planeMaterial.map.wrapS = planeMaterial.map.wrapT = THREE.RepeatWrapping;
+planeMaterial.map.repeat.set(10, 10);
+
 const grass = new THREE.Mesh(planeGeometry, planeMaterial);
 grass.receiveShadow = true;
 grass.rotation.x = -0.5 * Math.PI;
 scene.add(grass);
 
+
 // create light
-const light = new THREE.DirectionalLight(0xeeffd3, 10);
-light.castShadow = false;
-light.position.set(0, 50, 10);
+// const light = new THREE.DirectionalLight(0xeeffd3, 10);
+// light.castShadow = false;
+// light.position.set(0, 50, 10);
+// scene.add(light);
+
+// create light from above
+const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
 scene.add(light);
 
+
+// create fire for campfire, using volumetricFire script helper.
+const fire = new VolumetricFire(fireWidth = 3, fireHeight = 6, fireDepth = 3, sliceSpacing = 0.5, camera);
+scene.add(fire.mesh);
+
+// you can set position, rotation and scale
+// fire.mesh accepts THREE.mesh features
+
+fire.mesh.position.set( 0, fireHeight / 2, 0 );
+
+
+// load textures a single time for trees
+const trunkTexture = new THREE.TextureLoader().load('textures/trunk.jpg');
+const leaveTexture = new THREE.TextureLoader().load('textures/leaves.jpg');
+
 for (let i = 0; i < 500; i++) {
-  const trunk = renderTree();
-  scene.add(trunk);
+  renderTree();
 }
 
+
+// render campfire
 for (let i = 0; i < 50; i++) {
-  const rock = renderCampsite();
-  scene.add(rock);
+  renderCampsite();
+
 }
+
 
 function render() {
   requestAnimationFrame(render);
   controls.update();
+  fire.update(clock.getElapsedTime())
   renderer.render(scene, camera);
 }
 
