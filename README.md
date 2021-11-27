4a30e6d57178c72fd31022b8b60cd5b11e421c10
Mario Marroquin <a00825266@itesm.mx>
2021-11-27 07:00:57 -0600

    Add sound and campfire.
    Readme updated.
    Textures fixed.

--- README.md
+++ README.md
@@ -1,13 +1,39 @@
-# proyectoWebLG
+# proyecto - Bosque en la noche
+## Descripción
+El proyecto consiste en una escena representando un bosque durante la noche con una fogata prendida y utilizamos
+el framework de javascript ThreeJS (webGL) para crearlo.
 
-Para este render, crearemos una escena de una fogata en la noche al rededor de un bosque.
+La escena la dividimos en dos tipos de formas, las primitivas simples y complejas.
+Para los árboles utilizamos el cuadrado y/o rectángulo como figuras primitivas.
+Para la fogata utilizamos una aplicación para modelar los objetos (rocas y troncos).
 
-Usaremos el framework de javascript ThreeJS para nuestro proyecto.
+La camara de la escena puede ser controlada, pero siempre viendo hacia la fogata.
 
-Vamos a separar la escena en dos tipos de formas. Simples y complejas.
+Para simular el fuego de la fogata, reutilizamos el código de volumetricFire (que pertenece a yomotsu).
 
-Las formas complejas pueden ser el bosque, los arboles, plantas, etc... y para las primitivas podemos hacer piedras pequeñas, la tierra con una textura, el sacate.
+La escena cuenta con 3 luces:
+1. Luz de la fogata, que ilumina un área alrededor.
+2. Luz general para que los objetos se muestren.
+3. Luz direccional, para simular la luz de la luna en un área.
 
-Se podra mover la vista de la escena un poco para darle un poco mas de realismo pero solo algunos grados de rotacion para asi mantener que la escena se vea mas realista en vez de poder dar una vuelta 360.
+Para simular un cielo, utilizamos "SkyBox", nuestro plano dentro de él.
 
-Como tentativo usaremos particulas para simular partes del fuego y el humo que genera la fogata. Para la luz, utilizaremos la luz de la fogata y de la luna para iluminar la escena.
+La escena tiene audio de fondo y el audio de la fogata es posicional, entre más te
+acerques el sonido es más fuerte y viceversa.
+
+## Capturas
+![](capturas/BF1.png)
+![](capturas/BF2.png)
+![](capturas/BF3.png)
+![](capturas/BF4.png)
+![](capturas/BF5.png)
+
+## Referencias
+- [ThreeJS](https://threejs.org/).
+- VolumetricFire (Fogata) de [yomotsu](https://github.com/yomotsu/VolumetricFire).
+- SkyBox licencia [Sleepyhollow](http://creativecommons.org/licenses/by-sa/3.0/) por:
+Zachery "skiingpenguins" Slocum.
+
+## Autores
+- [@MarioMarroquin](https://github.com/MarioMarroquin)
+- [@FranciscoSainz](https://github.com/pacosw1)
--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+// Sky world
+const skyBox = () => {
+  // render Skybox
+  let SkyCube = [];
+  let texture_ft = new THREE.TextureLoader().load( 'sky/sleepyhollow_ft.jpg');
+  let texture_bk = new THREE.TextureLoader().load( 'sky/sleepyhollow_bk.jpg');
+  let texture_up = new THREE.TextureLoader().load( 'sky/sleepyhollow_up.jpg');
+  let texture_dn = new THREE.TextureLoader().load( 'sky/sleepyhollow_dn.jpg');
+  let texture_rt = new THREE.TextureLoader().load( 'sky/sleepyhollow_rt.jpg');
+  let texture_lf = new THREE.TextureLoader().load( 'sky/sleepyhollow_lf.jpg');
+
+  SkyCube.push(new THREE.MeshBasicMaterial( { map: texture_ft }));
+  SkyCube.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
+  SkyCube.push(new THREE.MeshBasicMaterial( { map: texture_up }));
+  SkyCube.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
+  SkyCube.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
+  SkyCube.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
+
+  for (let i = 0; i < 6; i++)
+    SkyCube[i].side = THREE.BackSide;
+
+  let skyboxGeo = new THREE.BoxGeometry( 10000, 10000, 10000);
+  let skybox = new THREE.Mesh( skyboxGeo, SkyCube );
+  scene.add(skybox);
+};
+
+// Spawn árboles
 const renderTree = () => {
   const circleRadius = 10;
 
@@ -19,21 +46,18 @@ const renderTree = () => {
   if (distance < circleRadius) return;
 
   trunk.position.set(treeX, 4, treeY);
-  trunk.scale.set(1, 8, 1);
+  trunk.scale.set(1.5, 12, 1.5);
 
-  leave.position.set(treeX, 8, treeY);
-  leave.scale.set(3, 3.5, 3);
+  leave.position.set(treeX, 12, treeY);
+  leave.scale.set(3, 5, 3);
 
   scene.add(trunk);
   scene.add(leave);
 };
 
+// Spawn fogata (piedras)
 const renderCampsite = () => {
-  const circleRadius = 3;
-
-  const stoneGeometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-  const stoneMaterial = new THREE.MeshPhongMaterial({ color: 0x3f330c });
-  const stone = new THREE.Mesh(stoneGeometry, stoneMaterial);
+  const circleRadius = 2;
 
   let randomSignX = Math.round(Math.random()) === 1 ? 1 : -1;
   let randomSignY = Math.round(Math.random()) === 1 ? 1 : -1;
@@ -44,12 +68,135 @@ const renderCampsite = () => {
 
   if (Math.floor(distance) !== circleRadius) return renderCampsite();
 
-  stone.position.set(treeX, 0, treeY);
+  // Se cargan el objeto y el material (creados con vectary).
+  const mtlLoader = new THREE.MTLLoader();
+  mtlLoader.load('textures/Stone.mtl', function (materials) {
+    materials.preload();
 
-  scene.add(stone);
+    const objLoader = new THREE.OBJLoader();
+    objLoader.setMaterials(materials);
+    objLoader.load('textures/Stone.obj', function (object) {
+      object.scale.set(0.1, 0.1, 0.1);
+      object.position.set(treeX, -0.1, treeY + 1);
+      scene.add(object);
+    });
+  });
 };
 
+// Crea los 4 troncos debajo del fuego. (creado con vectary).
+const renderCoal = () => {
+  const mtlLoader = new THREE.MTLLoader();
+  mtlLoader.load('textures/matWood.mtl', function (materials) {
+    materials.preload();
 
+    const objLoader = new THREE.OBJLoader();
+    objLoader.setMaterials(materials);
+
+    objLoader.load('textures/wood.obj', function (object) {
+      object.scale.set(0.3, 0.3, 0.3);
+      object.position.set(0, -0.1, -1);
+
+      const text = new THREE.TextureLoader().load('textures/grad.jpg');
+      object.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+          child.material.map = text;
+        }
+      });
+      scene.add(object);
+    });
+
+    objLoader.load('textures/wood.obj', function (object) {
+      object.scale.set(0.3, 0.3, 0.3);
+      object.position.set(0.5, -0.1, -1);
+
+      const text = new THREE.TextureLoader().load('textures/grad.jpg');
+      object.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+          child.material.map = text;
+        }
+      });
+      scene.add(object);
+    });
+
+    objLoader.load('textures/wood.obj', function (object) {
+      object.scale.set(0.3, 0.3, 0.3);
+      object.position.set(0.5, -0.1, -1);
+      object.rotation.x = 50;
+      object.rotation.y = 50;
+
+      const text = new THREE.TextureLoader().load('textures/grad.jpg');
+      object.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+          child.material.map = text;
+        }
+      });
+      scene.add(object);
+    });
+
+    objLoader.load('textures/wood.obj', function (object) {
+      object.scale.set(0.3, 0.3, 0.3);
+      object.position.set(-0.5, -0.1, -1);
+      object.rotation.x = 0;
+      object.rotation.y = 0;
+      object.rotation.z = 0;
+
+      const text = new THREE.TextureLoader().load('textures/grad.jpg');
+      object.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+          child.material.map = text;
+        }
+      });
+      scene.add(object);
+    });
+
+  });
+};
+
+const musica = () => {
+  // Pone la música de fondo.
+  const soundM = new THREE.Audio(listener);
+  const audioLoaderM = new THREE.AudioLoader();
+  audioLoaderM.load('sound/STAY.wav', function(buffer) {
+    soundM.setBuffer(buffer);
+    soundM.setLoop(true);
+    soundM.setVolume(0.2);
+    soundM.play();
+  });
+};
+
+const bosqueEfectos = () => {
+  // Pone los efectos del bosque con grillos.
+  const soundF = new THREE.Audio(listener);
+  const audioLoaderF = new THREE.AudioLoader();
+  audioLoaderF.load('sound/forest.wav', function(buffer) {
+    soundF.setBuffer(buffer);
+    soundF.setLoop(true);
+    soundF.setVolume(0.3);
+    soundF.play();
+  });
+};
+
+const fuegoEfectos = () => {
+  // Audio posicional, de acuerdo a distancia se oye más o menos la fogata.
+  const soundC = new THREE.PositionalAudio(listener);
+  const audioLoaderC = new THREE.AudioLoader();
+  audioLoaderC.load('sound/fire.wav', function(buffer) {
+    soundC.setBuffer(buffer);
+    soundC.setLoop(true);
+    soundC.setVolume(1);
+    soundC.setRefDistance(10);
+    soundC.play();
+  });
+
+  // La esfera simula la fogata para que el sonido parta de ahí.
+  const sphere = new THREE.SphereGeometry( 1, 1, 1 );
+  const material = new THREE.MeshPhongMaterial( { color: 0xff2200 } );
+  const mesh = new THREE.Mesh( sphere, material );
+  scene.add( mesh );
+  mesh.add( soundC );
+};
+
+// create scene
 const scene = new THREE.Scene();
 const clock = new THREE.Clock();
 
@@ -61,34 +208,36 @@ document.body.appendChild(renderer.domElement);
 
 
 // create camera
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    1,
-    10000
-);
-
-camera.position.z = 20;
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+camera.position.z = 25;
 camera.position.y = 40;
 camera.position.x = 0;
 
-
 // create control to move around scene, center fix.
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
-controls.minDistance = 5;
-controls.maxDistance = 30;
-controls.minPolarAngle = 0.8;
-controls.maxPolarAngle = 1.5;
+// Limitar lo que se puede mover la cámara
+controls.minDistance = 7;
+controls.maxDistance = 40;
+controls.minPolarAngle = 0.6;
+controls.maxPolarAngle = 1.44;
 controls.update();
 
+// create listener added to cam.
+const listener = new THREE.AudioListener();
+camera.add(listener);
 
-// create plane
+// Start sound effects and music
+musica();
+bosqueEfectos();
+fuegoEfectos();
+
+// create plane and render sky
 const planeDimensions = 100;
 const percentage = 0.97;
 
 const planeGeometry = new THREE.PlaneGeometry(100, 100, 50, 50);
 const planeTexture = new THREE.TextureLoader().load('textures/floor.jpg');
-const planeMaterial = new THREE.MeshPhongMaterial({ map: planeTexture});
+const planeMaterial = new THREE.MeshLambertMaterial({ map: planeTexture});
 planeMaterial.map.wrapS =  planeMaterial.map.wrapS = planeMaterial.map.wrapT = THREE.RepeatWrapping;
 planeMaterial.map.repeat.set(10, 10);
 
@@ -96,71 +245,43 @@ const grass = new THREE.Mesh(planeGeometry, planeMaterial);
 grass.receiveShadow = true;
 grass.rotation.x = -0.5 * Math.PI;
 scene.add(grass);
+skyBox();
 
 
-// create light
-// const light = new THREE.DirectionalLight(0xeeffd3, 10);
-// light.castShadow = false;
-// light.position.set(0, 50, 10);
-// scene.add(light);
+const lightGeneral = new THREE.AmbientLight( 0x545252 ); // iluminate scene
+scene.add(lightGeneral);
 
-// create light from above
-const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
-scene.add(light);
+const lightMoon = new THREE.PointLight( 0xB2B2B2, 2, 85 ); // Simular la luz de la luna
+lightMoon.position.set( 50, 50, 15 );
+scene.add(lightMoon);
+
+const lightFire = new THREE.PointLight( 0xE58300, 2, 30 ); // Luz que simula luz de fuego.
+lightFire.position.set(0, 2, 0);
+scene.add(lightFire);
 
 
 // create fire for campfire, using volumetricFire script helper.
 const fire = new VolumetricFire(fireWidth = 3, fireHeight = 6, fireDepth = 3, sliceSpacing = 0.5, camera);
 scene.add(fire.mesh);
-
-// you can set position, rotation and scale
-// fire.mesh accepts THREE.mesh features
-
 fire.mesh.position.set( 0, fireHeight / 2, 0 );
-
 
 
 // load textures a single time for trees
 const trunkTexture = new THREE.TextureLoader().load('textures/trunk.jpg');
-const leaveTexture = new THREE.TextureLoader().load('textures/leaves.jpg');
+const leaveTexture = new THREE.TextureLoader().load('textures/leafes.jpg');
 
 for (let i = 0; i < 500; i++) {
   renderTree();
 }
 
-
-
-
 // render campfire
 for (let i = 0; i < 50; i++) {
   renderCampsite();
-
 }
 
-var axisHelper = new THREE.AxisHelper( 5 );
-scene.add( axisHelper );
+// render fire wood.
+renderCoal();
 
-let materialArray = [];
-let texture_ft = new THREE.TextureLoader().load( 'sky/sleepyhollow_ft.jpg');
-let texture_bk = new THREE.TextureLoader().load( 'sky/sleepyhollow_bk.jpg');
-let texture_up = new THREE.TextureLoader().load( 'sky/sleepyhollow_up.jpg');
-let texture_dn = new THREE.TextureLoader().load( 'sky/sleepyhollow_dn.jpg');
-let texture_rt = new THREE.TextureLoader().load( 'sky/sleepyhollow_rt.jpg');
-let texture_lf = new THREE.TextureLoader().load( 'sky/sleepyhollow_lf.jpg');
-
-materialArray.push(new THREE.MeshBasicMaterial( { map: texture_ft }));
-materialArray.push(new THREE.MeshBasicMaterial( { map: texture_bk }));
-materialArray.push(new THREE.MeshBasicMaterial( { map: texture_up }));
-materialArray.push(new THREE.MeshBasicMaterial( { map: texture_dn }));
-materialArray.push(new THREE.MeshBasicMaterial( { map: texture_rt }));
-materialArray.push(new THREE.MeshBasicMaterial( { map: texture_lf }));
-
-for (let i = 0; i < 6; i++)
-  materialArray[i].side = THREE.BackSide;
-
-let skyboxGeo = new THREE.BoxGeometry( 10000, 10000, 10000);
-let skybox = new THREE.Mesh( skyboxGeo, materialArray );
-scene.add( skybox );
 
 function render() {
   requestAnimationFrame(render);
